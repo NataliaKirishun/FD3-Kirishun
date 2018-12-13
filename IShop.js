4d20b298a83e2a0df95a00cce46999964894f652
NataliaKirishun <31582169+NataliaKirishun@users.noreply.github.com>
2018-12-14 00:59:04 +0300

    iShop react app

---
+++ IShop.js
@@ -0,0 +1,33 @@
+﻿var IShop = React.createClass({
+
+  displayName: 'IShop',
+
+  propTypes: {
+    question: React.PropTypes.string.isRequired,
+
+    items:React.PropTypes.arrayOf(
+      React.PropTypes.shape({
+        name: React.PropTypes.string.isRequired,
+        price: React.PropTypes.number.isRequired,
+        count: React.PropTypes.number.isRequired,
+        url: React.PropTypes.string.isRequired
+      })
+    )
+
+  },
+
+  render: function() {
+
+    var answersCode=this.props.answers.map( v =>
+        React.DOM.div({key:v.code,className:'Answer'},
+          React.DOM.span({className:'Count'},v.count),
+          React.DOM.span({className:'Text'},v.text),
+        )
+      );
+    return React.DOM.div( {className:'VotesBlock'}, 
+      React.DOM.div( {className:'Question'}, this.props.question ),
+      React.DOM.div( {className:'Answers'}, answersCode ),
+    );
+  },
+
+});
