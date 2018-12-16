e8c74d031c5941f9d964d6232406bc91dc9e1a98
NataliaKirishun <31582169+NataliaKirishun@users.noreply.github.com>
2018-12-16 11:30:15 +0300

    webpack for the project

--- src/HW1/Item.js
+++ src/HW1/Item.js
@@ -13,7 +13,9 @@ var Item = React.createClass({
             React.DOM.h1({className: 'Title'}, this.props.item.name),
             React.DOM.img({className: 'Image', src: this.props.item.url, alt: this.props.item.name}),
             React.DOM.div({className: 'Price'}, this.props.item.price + '$'),
-            React.DOM.div({className: 'Count'}, this.props.item.count + 'шт.')
+            React.DOM.div({className: 'Count'}, this.props.item.count + 'шт.'),
+            React.DOM.button({className: 'Button'}, 'Buy now')
+
         );
     },
 
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom";
+import React from "react";
+
+import Hello from "./js/presentational/Hello";
+
+ReactDOM.render(<Hello name="Foo Bar"/>, document.querySelector('#root'));
+
+
