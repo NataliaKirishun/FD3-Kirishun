b07f234122b34de76c405f42d14db68f265a30ba
NataliaKirishun <31582169+NataliaKirishun@users.noreply.github.com>
2019-02-26 07:33:24 +0300

    Project

---
+++ Project/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {StoreModule} from "./store/store.module";
+import {StoreComponent} from "./store/store.component";
+import {CheckoutComponent} from "./store/checkout/checkout.component";
+import {CartDetailComponent} from "./store/cart-detail/cartDetail.component";
+import {RouterModule} from "@angular/router";
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule,
+    RouterModule.forRoot([
+      { path:"store", component: StoreComponent},
+      { path:"cart", component: CartDetailComponent},
+      { path:"checkout", component: CheckoutComponent},
+      { path:"**", redirectTo: "/store"}
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- Project/src/app/model/cart.model.ts
+++ Project/src/app/model/cart.model.ts
@@ -2,15 +2,60 @@ import {Injectable} from "@angular/core";
 import {Product} from "./product.model";
 
 @Injectable()
-export class Cart{
+export class Cart {
   public lines: CartLine[] = [];
-  public itemCount: number = 1;
+  public itemCount: number = 0;
   public cartPrice: number = 0;
+
+  addLine(product: Product, quantity: number = 1) {
+    let line = this.lines.find(line => line.product.id === product.id);
+    if (line !== undefined) {
+      line.qauntity += quantity;
+    } else {
+      this.lines.push(new CartLine(product, quantity))
+    }
+    this.recalculate();
+  }
+
+  private recalculate() {
+    this.itemCount = 0;
+    this.cartPrice = 0;
+    this.lines.forEach(line => {
+      this.itemCount += line.qauntity;
+      this.cartPrice += ( line.qauntity * line.product.price);
+    })
+  }
+
+  updateQuantity(product: Product, quantity: number) {
+    let line = this.lines.find( line => line.product.id ===product.id);
+    if (line) {
+      line.qauntity = quantity;
+    }
+    this.recalculate();
+  }
+
+  removeLine(id:number){
+    let index = this.lines.findIndex( line => line.product.id === id);
+    this.lines.splice(index, 1);
+    this.recalculate();
+  }
+
+  clear (){
+    this.lines = [];
+    this.itemCount = 0;
+    this.cartPrice = 0;
+  }
+
+
 };
 
 export class CartLine {
 
-  constructor( public product: Product, public qauntity: number ){}
+  constructor(public product: Product, public qauntity: number) {
+  }
 
+  get lineTotal() {
+    return this.qauntity * this.product.price;
+  }
 
 }
